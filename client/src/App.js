2dcc1f1adcc562909cc5f00fde4f6aee083e6fbd
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-04-06 12:52:29 -0600

    changed nav to bootstrap nav

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
       <div>
-        <Nav />
+        <Navbar />
         <Switch>
           <Route exact path="/" component={Search} />
           <Route exact path="/Search" component={Search} />
